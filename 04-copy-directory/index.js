0d4abbac3d89a86f9262746b2bbb8aa30731f02b
Alexey Nikitin <alexey.nikitin6723@gmail.com>
2022-05-24 22:57:48 +0400

    fix: update files-copy on restart in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,11 +1,12 @@
 const path = require('path');
-const { readdir, mkdir, copyFile } = require('fs/promises');
+const { readdir, mkdir, copyFile, rm } = require('fs/promises');
 
 const filePath = path.join(__dirname, 'files');
 const filePathCopy = path.join(__dirname, 'files-copy');
 
 async function copyDir(src, dest) {
 	try {
+		await rm(path.join(dest), { force: true, recursive: true });
 		const files = await readdir(src, { withFileTypes: true });
 		mkdir(dest, { recursive: true });
 		for (let file of files) {
