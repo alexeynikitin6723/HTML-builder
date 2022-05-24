c04ac0e1b1221d1e7926d10cca37117d196f61f6
Alexey Nikitin <alexey.nikitin6723@gmail.com>
2022-05-24 22:40:33 +0400

    fix: add phrase in 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,8 +6,12 @@ const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 stdout.write('Hello! Write here\n')
 stdin.on('data', chunk => {
 	if (chunk.toString().trim() == 'exit') {
+		console.log('Goodluck!');
 		process.exit();
 	}
 	output.write(chunk);
 });
-process.on('exit', () => stdout.write('Goodluck!'))
+process.on('SIGINT', () => {
+	stdout.write('Goodluck!');
+	process.exit();
+})
